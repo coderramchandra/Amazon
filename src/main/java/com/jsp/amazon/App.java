f1767f14080a06e7a8648576b6e7eae860b471de
coderramchandra <ramm2kk@gmail.com>
2023-12-14 14:16:58 +0530

    commiting src file

---
+++ src/main/java/com/jsp/amazon/App.java
@@ -0,0 +1,21 @@
+package com.jsp.amazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // change 1 made in master
+        // change made by dev 1
+        
+        
+        
+        
+        
+        
+    }
+}
